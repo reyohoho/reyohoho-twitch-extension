fc756a41358317ffb5417271f6eebb92f1b551be
reyohoho <reyohoho@proton.me>
2025-07-13 07:42:56 +0300

    Update icons

--- src/common/components/LogoIcon.jsx
+++ src/common/components/LogoIcon.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function LogoIcon(props) {
   return (
     <img 
-      src="https://starege.rhhhhhhh.live/https://cdn.7tv.app/emote/01FBDZWBCG00072B7YSZWSNQNK/1x.webp" 
+      src="https://starege.rhhhhhhh.live/badge.webp" 
       alt="ReYohoho Twitch Extension" 
       style={{ width: '100%', height: '100%', objectFit: 'contain' }}
       {...props}
