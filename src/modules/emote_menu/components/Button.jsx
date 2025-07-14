8675b4039a7b7bf6aec287c109caee61ce77c21b
reyohoho <reyohoho@proton.me>
2025-07-14 15:05:04 +0300

    Fix emote panel icon

--- src/modules/emote_menu/components/Button.jsx
+++ src/modules/emote_menu/components/Button.jsx
@@ -81,7 +81,36 @@ export default function Button({
         />
       }>
       {isLegacy ? (
-        <button type="button" className={classNames(styles.legacyButton, className)} />
+        <button type="button" className={classNames(styles.legacyButton, className)}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.5 47.5" style={{enableBackground: 'new 0 0 47.5 47.5'}} xmlSpace="preserve">
+            <g transform="matrix(1.25,0,0,-1.25,0,47.5)">
+              <g>
+                <g clipPath="url(#clipPath16)">
+                  <g transform="translate(36,19)">
+                    <path style={{fill: '#ffcc4d', fillOpacity: 1, fillRule: 'nonzero', stroke: 'none'}} d="m 0,0 c 0,-9.389 -7.611,-17 -17,-17 -9.389,0 -17,7.611 -17,17 0,9.389 7.611,17 17,17 C -7.611,17 0,9.389 0,0"/>
+                  </g>
+                  <g transform="translate(19,16)">
+                    <path style={{fill: '#664500', fillOpacity: 1, fillRule: 'nonzero', stroke: 'none'}} d="m 0,0 c -3.623,0 -6.027,0.422 -9,1 -0.679,0.131 -2,0 -2,-2 0,-4 4.595,-9 11,-9 6.404,0 11,5 11,9 C 11,1 9.679,1.132 9,1 6.027,0.422 3.623,0 0,0"/>
+                  </g>
+                  <g transform="translate(11,24)">
+                    <path style={{fill: '#664500', fillOpacity: 1, fillRule: 'nonzero', stroke: 'none'}} d="M 0,0 C 0,0 0,2 2,2 4,2 4,0 4,0 l 0,-2 c 0,0 0,-2 -2,-2 -2,0 -2,2 -2,2 l 0,2 z"/>
+                  </g>
+                  <g transform="translate(23,24)">
+                    <path style={{fill: '#664500', fillOpacity: 1, fillRule: 'nonzero', stroke: 'none'}} d="M 0,0 C 0,0 0,2 2,2 4,2 4,0 4,0 l 0,-2 c 0,0 0,-2 -2,-2 -2,0 -2,2 -2,2 l 0,2 z"/>
+                  </g>
+                  <g transform="translate(10,15)">
+                    <path style={{fill: '#ffffff', fillOpacity: 1, fillRule: 'nonzero', stroke: 'none'}} d="m 0,0 c 0,0 3,-1 9,-1 6,0 9,1 9,1 0,0 -2,-4 -9,-4 -7,0 -9,4 -9,4"/>
+                  </g>
+                </g>
+              </g>
+            </g>
+            <defs>
+              <clipPath id="clipPath16" clipPathUnits="userSpaceOnUse">
+                <path d="M 0,38 38,38 38,0 0,0 0,38 Z"/>
+              </clipPath>
+            </defs>
+          </svg>
+        </button>
       ) : (
         <button type="button" className={classNames(styles.button, className)}>
           <LogoIcon />
